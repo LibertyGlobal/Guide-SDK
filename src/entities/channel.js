0ee3ace2780f7cdd09ca4e88de44c766165930e3
Joris Snaras <jsnaras@lgi.com>
2013-06-21 17:25:10 +0200

    Added attributes to entities.

--- src/entities/channel.js
+++ src/entities/channel.js
@@ -11,6 +11,13 @@
         this._baseURL = 'channels.json?';
     }
 
+    kraken.Channel.ID = new kraken.TextField('id');
+    kraken.Channel.NAME = new kraken.TextField('name');
+    kraken.Channel.LOGICAL_POSITION = new kraken.NumericField('logicalPosition');
+    kraken.Channel.SELF_LINK = new kraken.TextField('selfLink');
+    kraken.Channel.BROADCASTS_LINK = new kraken.TextField('broadcastsLink');
+    kraken.Channel.OPENGRAPH_LINK = new kraken.TextField('opengraphLink');
+
     kraken.utils.addFactory(kraken.Channel);
 
     var p = kraken.Channel.prototype = Object.create(kraken.EntityBase.prototype);
--- src/entities/country.js
+++ src/entities/country.js
@@ -11,6 +11,13 @@
         this._baseURL = 'regions.json';
     }
 
+    kraken.Country.ID = new kraken.TextField('id');
+    kraken.Country.NAME = new kraken.TextField('name');
+    kraken.Country.CHANNEL_LINEUP_LINK = new kraken.TextField('channelLineupLink');
+    kraken.Country.SELF_LINK = new kraken.TextField('selfLink');
+    kraken.Country.TOP_BROADCASTS_LINK = new kraken.TextField('topBroadcastsLink');
+    kraken.Country.TOP_VIDEOS_LINK = new kraken.TextField('topVideosLink');
+
     kraken.utils.addFactory(kraken.Country);
 
     var p = kraken.Country.prototype = Object.create(kraken.EntityBase.prototype);
