6bc4543406ac80d94da3b78077843ef192ddb32b
PixelsCommander <denis.radin@gmail.com>
2013-06-24 11:54:27 +0200

    Text/Numeric fields doc added

--- src/fields/numericfield.js
+++ src/fields/numericfield.js
@@ -12,22 +12,47 @@
 
     var p = kraken.NumericField.prototype = Object.create(kraken.AbstractField.prototype);
 
+    /**
+     * Adds = filtering operation.
+     * @method NumericField#equalTo
+     * @param operand
+     */
     p.equalTo = function (operand) {
         return this.getStringForOperation('=', operand);
     };
 
+    /**
+     * Adds > filtering operation.
+     * @method NumericField#greaterThan
+     * @param operand
+     */
     p.greaterThan = function (operand) {
         return this.getStringForOperation('>', operand);
     };
 
+    /**
+     * Adds >= filtering operation.
+     * @method NumericField#greaterThan
+     * @param operand
+     */
     p.greaterThanOrEqualTo = function (operand) {
         return this.getStringForOperation('>=', operand);
     };
 
+    /**
+     * Adds < filtering operation.
+     * @method NumericField#lessThan
+     * @param operand
+     */
     p.lessThan = function (operand) {
         return this.getStringForOperation('<', operand);
     };
 
+    /**
+     * Adds <= filtering operation.
+     * @method NumericField#lessThanOrEqualTo
+     * @param operand
+     */
     p.lessThanOrEqualTo = function (operand) {
         return this.getStringForOperation('<=', operand);
     };
--- src/fields/textfield.js
+++ src/fields/textfield.js
@@ -12,10 +12,20 @@
 
     var p = kraken.TextField.prototype = Object.create(kraken.AbstractField.prototype);
 
+    /**
+     * Adds = filtering operation.
+     * @method TextField#equalTo
+     * @param operand
+     */
     p.equalTo = function (operand) {
         return this.getStringForOperation('=', operand);
     }
 
+    /**
+     * Adds ~ filtering operation. Says to backend to filter records by regexp.
+     * @method TextField#equalTo
+     * @param operand
+     */
     p.isMatching = function (operand) {
         return this.getStringForOperation('~', operand);
     }
