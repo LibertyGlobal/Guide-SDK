9d25989ad82c71b0b7994b04f5fd6333d9121f82
Joris Snaras <jsnaras@lgi.com>
2013-06-24 12:14:12 +0200

    Added Jasmine test runner plugin, Istanbul code coverage, auto-test via watch; modified package.json accordingly.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,41 +2,50 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            build: {
-                src: [
-                    'src/core.js',
-                    'src/config.js',
-                    'src/utils/utils.js',
-                    'src/utils/jsonp.js',
-                    'src/utils/polyfills.js',
-                    'src/fields/abstractfield.js',
-                    'src/fields/numericfield.js',
-                    'src/fields/textfield.js',
-                    'src/request.js',
-                    'src/collection.js',
-                    'src/entitybase.js',
-                    'src/entities/country.js',
-                    'src/entities/city.js',
-                    'src/entities/channel.js',
-                    'src/entities/broadcast.js'
-                ],
-                dest: './lib/<%= pkg.name %>.js'
+        meta: {
+            pkg: grunt.file.readJSON('package.json'),
+            lib: 'lib',
+            src: 'src',
+            test: 'test',
+            spec: '<%= meta.test %>/spec',
+            dist: 'dist',
+            report: 'report',
+            banner: [
+                '// <%= meta.pkg.title || meta.pkg.name %>',
+                '// ----------------------------------',
+                '// v<%= meta.pkg.version %>',
+                '//',
+                '// Copyright (c) <%= grunt.template.today("yyyy") %> <%= meta.pkg.author %>',
+                '// Distributed under BSD license',
+                '\n'
+            ].join('\n'),
+            files: [
+                'src/core.js',
+                'src/config.js',
+                'src/utils/utils.js',
+                'src/utils/jsonp.js',
+                'src/utils/polyfills.js',
+                'src/fields/abstractfield.js',
+                'src/fields/numericfield.js',
+                'src/fields/textfield.js',
+                'src/request.js',
+                'src/collection.js',
+                'src/entitybase.js',
+                'src/entities/country.js',
+                'src/entities/city.js',
+                'src/entities/channel.js',
+                'src/entities/broadcast.js'
+            ]
+        },
+
+        clean: {
+            dist: {
+                files: [
+                    { src: [ '<%= meta.dist %>/*' ], dot: true }
+                ]
             }
         },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'lib/<%= pkg.name %>.js',
-                dest: 'lib/<%= pkg.name %>.min.js'
-            }
-        },
+
         jshint: {
             files: ['src/**/*.js'],
             options: {
@@ -49,6 +58,61 @@ module.exports = function (grunt) {
                 }
             }
         },
+
+        jasmine: {
+            options: {
+                specs: '<%= meta.spec %>/**/*.spec.js',
+                helpers: '<%= meta.test %>/helpers/*.js',
+                keepRunner: true,
+                outfile: 'SpecRunner.html'
+            },
+
+            coverage: {
+                src: '<%= jasmine.all.src %>',
+                options: {
+                    template: require('grunt-template-jasmine-istanbul'),
+                    templateOptions: {
+                        coverage: '<%= meta.report %>/coverage.json',
+                        report: '<%= meta.report %>/coverage'
+                    }
+                }
+            },
+
+            all: {
+                src: '<%= meta.files %>'
+            }
+        },
+
+        watch: {
+            all: {
+                files: [ '<%= meta.src %>/**/*.js', '<%= meta.spec %>/**/*.js' ],
+                tasks: [ 'test', 'build' ]
+            }
+        },
+
+        concat: {
+            options: {
+                separator: '\n\n',
+                banner: '<%= meta.banner %>'
+            },
+
+            build: {
+                src: '<%= meta.files %>',
+                dest: '<%= meta.dist %>/<%= meta.pkg.name %>.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+
+            build: {
+                src: '<%= meta.dist %>/<%= meta.pkg.name %>.js',
+                dest: '<%= meta.dist %>/<%= meta.pkg.name %>.min.js'
+            }
+        },
+
         jsdoc: {
             dist: {
                 src: ['src/**/*.js', 'test/*.js'],
@@ -56,22 +120,38 @@ module.exports = function (grunt) {
                     destination: 'doc'
                 }
             }
-        },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint', 'concat', 'uglify']
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-    // Default task(s).
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('test', [
+        'jshint',
+        'jasmine:all'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean',
+        'concat',
+        'uglify'
+    ]);
+
+    grunt.registerTask('dev', [
+        'test',
+        'watch:all'
+    ]);
+
+    grunt.registerTask('default', [
+        'jshint',
+        'jasmine:coverage',
+        'build'
+    ]);
+
     grunt.registerTask('doc', ['jshint', 'jsdoc', 'concat', 'uglify']);
-
 };
